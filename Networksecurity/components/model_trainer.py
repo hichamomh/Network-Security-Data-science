d7e8cafb8386e9222f7667271f309dde3c99cade
hichamomh <hichamoumouhou@gmail.com>
2024-11-16 18:23:53 +0000

    tracking experiment with mlflow

--- Networksecurity/components/model_trainer.py
+++ Networksecurity/components/model_trainer.py
@@ -46,7 +46,7 @@ class ModelTrainer:
             raise NetworkSecurityException(e,sys)
         
     def track_mlflow(self,best_model,classificationmetric):
-        mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
+        #mlflow.set_registry_uri("https://dagshub.com/krishnaik06/networksecurity.mlflow")
         tracking_url_type_store = urlparse(mlflow.get_tracking_uri()).scheme
         with mlflow.start_run():
             f1_score=classificationmetric.f1_score
@@ -76,9 +76,9 @@ class ModelTrainer:
         models = {
                 "Random Forest": RandomForestClassifier(verbose=1),
                 "Decision Tree": DecisionTreeClassifier(),
-                "Gradient Boosting": GradientBoostingClassifier(verbose=1),
-                "Logistic Regression": LogisticRegression(verbose=1),
-                "AdaBoost": AdaBoostClassifier(),
+                # "Gradient Boosting": GradientBoostingClassifier(verbose=1),
+                # "Logistic Regression": LogisticRegression(verbose=1),
+                # "AdaBoost": AdaBoostClassifier(),
             }
         params={
             "Decision Tree": {
@@ -92,19 +92,19 @@ class ModelTrainer:
                 # 'max_features':['sqrt','log2',None],
                 'n_estimators': [8,16,32,128,256]
             },
-            "Gradient Boosting":{
-                # 'loss':['log_loss', 'exponential'],
-                'learning_rate':[.1,.01,.05,.001],
-                'subsample':[0.6,0.7,0.75,0.85,0.9],
-                # 'criterion':['squared_error', 'friedman_mse'],
-                # 'max_features':['auto','sqrt','log2'],
-                'n_estimators': [8,16,32,64,128,256]
-            },
-            "Logistic Regression":{},
-            "AdaBoost":{
-                'learning_rate':[.1,.01,.001],
-                'n_estimators': [8,16,32,64,128,256]
-            }
+            # "Gradient Boosting":{
+            #     # 'loss':['log_loss', 'exponential'],
+            #     'learning_rate':[.1,.01,.05,.001],
+            #     'subsample':[0.6,0.7,0.75,0.85,0.9],
+            #     # 'criterion':['squared_error', 'friedman_mse'],
+            #     # 'max_features':['auto','sqrt','log2'],
+            #     'n_estimators': [8,16,32,64,128,256]
+            # },
+            # "Logistic Regression":{},
+            # "AdaBoost":{
+            #     'learning_rate':[.1,.01,.001],
+            #     'n_estimators': [8,16,32,64,128,256]
+            # }
             
         }
         model_report:dict=evaluate_models(X_train=X_train,y_train=y_train,X_test=x_test,y_test=y_test,
@@ -124,7 +124,7 @@ class ModelTrainer:
         classification_train_metric=get_classification_score(y_true=y_train,y_pred=y_train_pred)
         
         ## Track the experiements with mlflow
-        #self.track_mlflow(best_model,classification_train_metric)
+        self.track_mlflow(best_model,classification_train_metric)
 
 
         y_test_pred=best_model.predict(x_test)
