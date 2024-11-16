078a5b5e1d12ac2535505758b02dc5242b8a7ca0
hichamomh <hichamoumouhou@gmail.com>
2024-11-16 16:46:51 +0000

    add the training pipeline

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from Networksecurity.components.data_ingestion import DataIngestion
 from Networksecurity.components.data_validation import DataValidation
 from Networksecurity.components.data_transformation import DataTransformation
+from Networksecurity.components.model_trainer import ModelTrainer
 
-from Networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig, DataTransformationConfig
+from Networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig, DataTransformationConfig, ModelTrainerConfig
 from Networksecurity.exception.exception import NetworkSecurityException
 from Networksecurity.logging.logger import logging
 import sys
@@ -29,6 +30,13 @@ if __name__=='__main__':
         data_transformation_artifact=data_transformation.initiate_data_transformation()
         print(data_transformation_artifact)
         logging.info("data Transformation completed")
-    
+
+        logging.info("Model Training stared")
+        model_trainer_config=ModelTrainerConfig(trainingpipelineconfig)
+        model_trainer=ModelTrainer(model_trainer_config=model_trainer_config,data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact=model_trainer.initiate_model_trainer()
+        print(model_trainer_artifact)
+
+        logging.info("Model Training artifact created")
     except Exception as e:
            raise NetworkSecurityException(e,sys)
