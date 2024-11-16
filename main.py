543e7d001ba16ceb4a9f8ab96f33b53042a1a75d
hichamomh <hichamoumouhou@gmail.com>
2024-11-16 13:51:25 +0000

    add data validation pipeline

--- main.py
+++ main.py
@@ -1,16 +1,26 @@
-# from Networksecurity.components.data_ingestion import DataIngestion
-# from Networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig
-# from Networksecurity.exception.exception import NetworkSecurityException
-# from Networksecurity.logging.logger import logging
-# import sys
-# ## Initiate the Data Ingestion
+from Networksecurity.components.data_ingestion import DataIngestion
+from Networksecurity.components.data_validation import DataValidation
+from Networksecurity.entity.config_entity import DataIngestionConfig,TrainingPipelineConfig,DataValidationConfig
+from Networksecurity.exception.exception import NetworkSecurityException
+from Networksecurity.logging.logger import logging
+import sys
+## Initiate the Data Ingestion
 
-# if __name__=='__main__':
-#     try:
-#         logging.info("Enter the try block")
-#         data_ingestion_config = DataIngestionConfig(TrainingPipelineConfig())
-#         data_ingestion = DataIngestion(data_ingestion_config)
-#         data_ingestion.initiate_data_ingestion()
+if __name__=='__main__':
+    try:
+        trainingpipelineconfig=TrainingPipelineConfig()
+        dataingestionconfig=DataIngestionConfig(trainingpipelineconfig)
+        data_ingestion=DataIngestion(dataingestionconfig)
+        logging.info("Initiate the data ingestion")
+        dataingestionartifact=data_ingestion.initiate_data_ingestion()
+        logging.info("Data Initiation Completed")
+        print(dataingestionartifact)
+        data_validation_config=DataValidationConfig(trainingpipelineconfig)
+        data_validation=DataValidation(dataingestionartifact,data_validation_config)
+        logging.info("Initiate the data Validation")
+        data_validation_artifact=data_validation.initiate_data_validation()
+        logging.info("data Validation Completed")
+        print(data_validation_artifact)
     
-#     except Exception as e:
-#            raise NetworkSecurityException(e,sys)
+    except Exception as e:
+           raise NetworkSecurityException(e,sys)
